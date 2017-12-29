8366eee23b9e15caa525c586330b3b2944a70bfc
Shreyas Lad <shadowtemplates@gmail.com>
2017-12-29 11:57:21 -0800

    Added Mac App build
    Yay, Planets is now on Macs

---
+++ PlanetsMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PlanetsMac
+//
+//  Created by Shreyas Lad on 12/29/17.
+//  Copyright © 2017 Shreyas Lad. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
